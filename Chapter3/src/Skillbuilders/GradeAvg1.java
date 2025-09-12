40f76c2e73d179d9ee06d00af6f3a6d6c55587ae
49358507 <49358507@L815WD220008.edu.cbe.ab.ca>
2025-09-12 10:47:09 -0600

    10:47AM

---
+++ Chapter3/src/Skillbuilders/GradeAvg1.java
@@ -0,0 +1,43 @@
+package Skillbuilders;
+
+import java.util.Scanner;
+
+public class GradeAvg1 
+{
+
+	public static void main(String[] args) 
+	{
+		//Declare variables
+		double num1;
+		double num2;
+		double num3;
+		double num4;
+		double num5;
+		double average;
+		
+		Scanner input = new Scanner(System.in);
+			
+		//Prompt user to enter 5 grades
+		System.out.print ("Enter grade 1: ");
+		num1 = input.nextInt();
+		
+		System.out.print ("Enter grade 2: ");
+		num2 = input.nextInt();
+		
+		System.out.print ("Enter grade 3: ");
+		num3 = input.nextInt();
+		
+		System.out.print ("Enter grade 4: ");
+		num4 = input.nextInt();
+		
+		System.out.print ("Enter grade 5: ");
+		num5 = input.nextInt();
+		
+		//Calculate grade average
+		average = (num1 + num2 + num3 + num4 + num5)/ 5;
+
+		//Display Average
+		System.out.print("The average of your grades is" + average "%");
+	}
+
+}
