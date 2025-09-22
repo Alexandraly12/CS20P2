d058b8f423472c99ff380a3e51c607154b3d2dec
49358507 <49358507@L815WD220008.edu.cbe.ab.ca>
2025-09-22 11:56:25 -0600

    11:55AM

---
+++ Chapter4/src/Skillbuilders/Hurricane.java
@@ -0,0 +1,25 @@
+package Skillbuilders;
+
+import java.util.Scanner;
+
+public class Hurricane 
+{
+
+	public static void main(String[] args) 
+	{
+		 int hurricane;
+		 Scanner input = new Scanner(System.in);
+		 
+		 System.out.print("Please enter the hurricane category (1 - 5): ");
+		 hurricane = input.nextInt();
+		 
+		 if (hurricane = 1)
+	}
+
+}
+
+
+
+
+//Make user enter category of hurricane from 1 - 5
+//Create if thing where if user enter 1 display.., if user enter 2 display..., etc.
