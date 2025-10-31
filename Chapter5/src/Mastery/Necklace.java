9c0f48399603248f7740b31b2fece4797790903b
49358507 <49358507@L815WD220008.edu.cbe.ab.ca>
2025-10-31 10:45:30 -0600

    10:45AM

---
+++ Chapter5/src/Mastery/Necklace.java
@@ -0,0 +1,36 @@
+package Mastery;
+
+import java.util.Scanner;
+
+public class Necklace {
+
+	public static void main(String[] args) 
+	
+	{	
+		int sum;
+		int digit;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Please enter the first digit:");
+		int num1 = input.nextInt();
+		
+		System.out.print("Please enter the second digit:");
+		int num2 = input.nextInt();
+		
+		System.out.print(num1 + " " + num2);
+	
+		digit = num1 + num2;
+		sum = digit % 10;
+		
+		while (digit != num1 || digit !=num2); {
+			System.out.print(sum);
+			
+		}
+		
+		
+		
+		input.close();
+
+	}
+
+}
