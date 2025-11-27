4387d6f503dbe894a59a56b0bb3f752e64ce6060
49358507 <49358507@L815WD220008.edu.cbe.ab.ca>
2025-11-27 10:09:42 -0700

    10:09AM

--- Chapter7/src/Mastery/Nim2.java
+++ Chapter7/src/Mastery/Nim2.java
@@ -3,4 +3,33 @@ package Mastery;
 public class Nim2 
 {
 	
+	public Nim2()
+	{
+		
+	}
+	
+	public Nim2(int a)
+	{
+		
+	}
+	
+	public start()
+	{
+		
+	}
+	
+	public userplayer()
+	{
+		
+	}
+	
+	public comPlayer()
+	{
+		
+	}
+	
+	public String toString()
+	{
+		
+	}
 }
