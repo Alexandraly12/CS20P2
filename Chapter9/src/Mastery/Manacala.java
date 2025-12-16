1efe7a73a266a9f4d2321bb159d6c404d25dff99
49358507 <49358507@L815WD220008.edu.cbe.ab.ca>
2025-12-16 10:01:36 -0700

    10:01AM

--- Chapter9/src/Mastery/Manacala.java
+++ Chapter9/src/Mastery/Manacala.java
@@ -10,6 +10,10 @@ public class Manacala {
 		boolean playerTurn = true;
 		Scanner input = new Scanner(System.in);
 		char move;
+		int pit = 1;
+		int in;
+		boolean player1Empty = false;
+		boolean player2Empty = false;
 		
 		//Set up game
 		for(int i =0; i < board.length; i++)
@@ -22,94 +26,151 @@ public class Manacala {
 		
 		//Formatting board
 		//top row (player 2)
-		System.out.print("     ");
-		for(int i = 12; i >=7; i--)
-		{	
-			if(board[i] >= 10)
-			{
-			System.out.print("[" + board[i] + "] ");
+		do
+		{
+			System.out.print("     ");
+			for(int i = 12; i >=7; i--)
+			{	
+				if(board[i] >= 10)
+				{
+				System.out.print("[" + board[i] + "] ");
+				}
+				
+				else
+				{
+					System.out.print("[ " + board[i] + "] ");
+				}
 			}
 			
+			//Home pits
+			if(board[6] >= 10 && board[13] >=10) // If both pits have more than 10 stones
+			{
+				System.out.println("\n"
+							+ "[" + board[6] + "]                               [" + board[13] + "]");
+			}
+			else if( board[6] <=10 && board[13] <=10) // If both pits have less than 10 stones
+			{
+				System.out.println("\n"
+						+ "[ " + board[6] + "]                               [ " + board[13] + "]");
+			}
+			//If one pit has more and one pit has less than 10
+			else if(board[6] <=10 && board[13] >=10)
+			{
+				System.out.println("\n"
+						+ "[ " + board[6] + "]                               [" + board[13] + "]");
+			}
 			else
 			{
-				System.out.print("[ " + board[i] + "] ");
-			}
-		}
-		
-		//Home pits
-		if(board[6] >= 10 && board[13] >=10) // If both pits have more than 10 stones
-		{
-			System.out.println("\n"
-						+ "[" + board[6] + "]                               [" + board[13] + "]");
-		}
-		else if( board[6] <=10 && board[13] <=10) // If both pits have less than 10 stones
-		{
-			System.out.println("\n"
-					+ "[ " + board[6] + "]                               [ " + board[13] + "]");
-		}
-		//If one pit has more and one pit has less than 10
-		else if(board[6] <=10 && board[13] >=10)
-		{
-			System.out.println("\n"
-					+ "[ " + board[6] + "]                               [" + board[13] + "]");
-		}
-		else
-		{
-			System.out.println("\n"
-						+ "[" + board[6] + "]                               [ " + board[13] + "]");
-		}
-		
-		//Bottom row (player 1)
-		System.out.print("     ");
-		for(int i = 0; i <=5; i++)
-		{
-			if(board[i] >= 10)
-			{
-			System.out.print("[" + board[i] + "] ");
+				System.out.println("\n"
+							+ "[" + board[6] + "]                               [ " + board[13] + "]");
 			}
 			
-			else
+			//Bottom row (player 1)
+			System.out.print("     ");
+			for(int i = 0; i <=5; i++)
 			{
-				System.out.print("[ " + board[i] + "] ");
-			}
-		}
-		
-		//Letters for user to enter
-		System.out.print("\n       ");
-		for(int i = 0; i < 6; i ++)
-		{
-			System.out.print((char)('A' + i) + "    ");
-		}
-	
-		//Player 1 game play
-		System.out.print("\n");
-		do {
-		System.out.print("Player 1: Which pit would you like to move? (A - F)");
-		move = input.next().toUpperCase().charAt(0);
-		int pit = move - 'A';
-		System.out.print(board[3]);
-		
-		//Validate move
-		if(pit < 0 || pit > 5)
-		{
-			System.out.println("Invalid move. Choose pit A - F .");
-		}
-		
-		else if(board[pit] == 0)
-		{
-			System.out.print("That pit is empty. Reselect pit A - F");
-		}
-		
-		
-		for(int i = 0; i >= move; i++)
-			{
-				System.out.print(board[i] + i);
+				if(board[i] >= 10)
+				{
+				System.out.print("[" + board[i] + "] ");
+				}
+				
+				else
+				{
+					System.out.print("[ " + board[i] + "] ");
+				}
 			}
 		
-		}while(board[1] != 0);
-		
 	
+			//Player 1 game play
+			while(playerTurn == true)
+			{
+				//Letters for user to enter
+				System.out.print("\n       ");
+				for(int i = 0; i < 6; i ++)
+				{
+					System.out.print((char)('A' + i) + "    ");
+				}
+				
+				//User input
+				System.out.print("\n");
+				System.out.print("Player 1: Which pit would you like to move? (A - F)");
+				move = input.next().toUpperCase().charAt(0);
+				pit = move - 'A';
+				
+				//Validate move
+				if(pit < 0 || pit > 5)
+				{
+					System.out.println("Invalid move. Choose pit A - F .");
+				}
+				
+				else if(board[pit] == 0)
+				{
+					System.out.print("That pit is empty. Reselect pit A - F");
+				}
+				
+				int stones = board[pit];
+				board[pit] = 0;
+				in = pit;
+				
+				//Move stones into new pits
+				while(stones > 0)
+				{
+					in = (in + 1) % 14;
+					if(in != 13)
+					{
+						board[in]++;
+						stones--;
+					}
+				}
+				
+				playerTurn = false;
+		
+			}
+			
+			//Player 2
+			while(playerTurn == false)
+			{
+				//Letters for user to enter
+				System.out.print("\n       ");
+				for(int i = 7; i < 12; i --)
+				{
+					System.out.print((char)('F' + i) + "    ");
+				}
+				
+				//User input
+				System.out.print("Player 2: Which pit would you like to move? (A - F)");
+				move = input.next().toUpperCase().charAt(0);
+				pit = move - 'A';
+				
+				//Validate move
+				if(pit < 7 || pit > 12)
+				{
+					System.out.println("Invalid move. Choose pit A - F .");
+				}
+				else if(board[pit] == 0)
+				{
+					System.out.print("That pit is empty. Reselect pit A - F");
+				}
+				
+				int stones = board[pit];
+				board[pit] = 0;
+				in = pit;
+				
+				while(stones > 0)
+				{
+					in = (in + 1) % 14;
+					if(in != 13)
+					{
+						board[in]++;
+						stones--;
+					}
+				}
+				
+				playerTurn = true;
+			}
+			
+				
+		}while(player1Empty == true || player2Empty == true);
 
 	}
-
 }
---
+++ Chapter9/src/Mastery/countConstants.java
@@ -0,0 +1,11 @@
+package Mastery;
+
+public class countConstants {
+
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
